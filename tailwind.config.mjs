2b2706b462c92908a4dbd0f1d328f0ec9914ed91
repollodev <gabrielrepolloez@gmail.com>
2024-09-12 18:58:47 -0400

    🐈 Agregado de Catppuccin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,47 @@
-const { addIconSelectors } = require('@iconify/tailwind');
+const { addIconSelectors } = require("@iconify/tailwind");
+
+import { flavors } from "@catppuccin/palette";
+let { latte, mocha } = flavors;
+function mapColors(colorsObj) {
+  let scheme = {
+    primary: "sapphire",
+    secondary: "mauve",
+    accent: "yellow",
+    neutral: "text",
+    "base-100": "surface0",
+    "base-200": "surface1",
+    "base-300": "surface2",
+    "base-content": "text",
+    "neutral-content": "base",
+    info: "blue",
+    success: "green",
+    warning: "peach",
+    error: "maroon",
+  };
+  for (let key in scheme) {
+    scheme[key] = colorsObj[scheme[key]]["hex"];
+  }
+  return scheme;
+}
+latte = mapColors(latte.colors);
+mocha = mapColors(mocha.colors);
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('daisyui'),
-		addIconSelectors(['ph'])
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [require("daisyui"), addIconSelectors(["ph"])],
+  daisyui: {
+    themes: [
+      {
+        latte,
+      },
+      {
+        mocha,
+      },
+    ],
+  },
+};
