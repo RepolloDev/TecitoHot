5a2fcf30cfeefdc6e5ccbaf598f750c1db2914c4
repollodev <gabrielrepolloez@gmail.com>
2024-09-11 20:07:39 -0400

    👌 Agregado de iconify con phosphoricons para tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const { addIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -6,5 +8,6 @@ export default {
 	},
 	plugins: [
 		require('daisyui'),
+		addIconSelectors(['ph'])
 	],
 }
