b5ce4d87166648dea35d42fb2cbe99ea9ebbe08f
repollodev <gabrielrepolloez@gmail.com>
2024-09-22 15:14:35 -0400

    Agregado de cover por defecto para posts

--- src/content/posts/scheme.ts
+++ src/content/posts/scheme.ts
@@ -12,5 +12,5 @@ export const postsScheme = z.object({
     .refine((authors) => authors.length > 0, {
       message: "Post must have at least one author",
     }),
-  cover: z.string().url().optional(),
+  cover: z.string().url().default("https://ajcbxgvlgngaewqiinmg.supabase.co/storage/v1/object/sign/posts/fallBackCover.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3N0cy9mYWxsQmFja0NvdmVyLndlYnAiLCJpYXQiOjE3MjcwMzIzNDUsImV4cCI6MTc1ODU2ODM0NX0.0waspH3yZcl8YNWJuOmv_pvUUL62CmPeedJ65l6unyg&t=2024-09-22T19%3A12%3A24.426Z"),
 });
