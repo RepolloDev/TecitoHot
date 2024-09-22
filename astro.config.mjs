b5be679cf8e17f7d5a4e1d9d751972610e8e167b
repollodev <gabrielrepolloez@gmail.com>
2024-09-22 15:31:16 -0400

    Cambio de configuracion para el build de forma estatica

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   integrations: [tailwind(), mdx()],
   site: "https://repollodev.github.io",
   base: "TecitoHot",
