7cd6b468b6e3b6f0bb71f52b569e5e7a365b3108
repollodev <gabrielrepolloez@gmail.com>
2024-09-23 21:16:37 -0400

    Agregado de remark-math y mathjax para markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkGithubAlerts from "remark-gh-alerts";
+import remarkMath from "remark-math";
+import rehypeMathJax from "rehype-mathjax";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +13,7 @@ export default defineConfig({
   site: "https://repollodev.github.io",
   base: "TecitoHot",
   markdown: {
-    remarkPlugins: [remarkGithubAlerts],
+    remarkPlugins: [remarkGithubAlerts, remarkMath],
+    rehypePlugins: [rehypeMathJax],
   },
 });
